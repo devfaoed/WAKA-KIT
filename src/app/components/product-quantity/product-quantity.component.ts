6050cc846c4aeb9a73d03ad58a6a89102f4f5108
devfaoed <faithadedokun2018@gmail.com>
2023-03-09 16:48:32 -0800

    adding corrections

---
+++ src/app/components/product-quantity/product-quantity.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Product } from 'src/app/model/product';
+import { ShoppingCartService } from 'src/app/services/shopping-cart.service';
+
+@Component({
+  selector: 'product-quantity',
+  templateUrl: './product-quantity.component.html',
+  styleUrls: ['./product-quantity.component.scss']
+})
+export class ProductQuantityComponent {
+  @Input('product') product: Product;
+  @Input('shopping-cart') shoppingCart;
+
+  constructor(private cartService: ShoppingCartService) { }
+
+  addToCart() {
+    this.cartService.addToCart(this.product); 
+    alert("product increased successfully")
+  }
+
+  removeFromCart() {
+    this.cartService.removeFromCart(this.product); 
+    alert("product removed successfully")
+  }
+ 
+}
